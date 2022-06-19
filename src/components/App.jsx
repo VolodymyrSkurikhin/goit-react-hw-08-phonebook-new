4c1c79108ebebcb514f5fac47482480e1f2e07f1
VolodymyrSkurikhin <vladiskuri@gmail.com>
2022-06-19 11:03:26 +0300

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,15 +6,6 @@ import { ContactList } from './ContactList/ContactList';
 import s from './App.module.css';
 
 function App() {
-  // state = {
-  //   contacts: [
-  //     { id: '1', name: 'Rosie Simpson', number: '459-12-56' },
-  //     { id: '2', name: 'Hermione Kline', number: '443-89-12' },
-  //     { id: '3', name: 'Eden Clements', number: '645-17-79' },
-  //     { id: '4', name: 'Annie Copeland', number: '227-91-26' },
-  //   ],
-  //   filter: '',
-  // };
   const [contacts, setContacts] = useState(() => {
     const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
     if (parsedContacts) {
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,69 +1,67 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import s from './ContactForm.module.css';
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+function ContactForm({ onSubmit }) {
+  // state = {
+  //   name: '',
+  //   number: '',
+  // };
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handleInput = e => {
-    this.setState({ [e.currentTarget.name]: e.currentTarget.value });
-  };
-  handleSubmit = e => {
+  // handleNameInput = e => {
+  //   this.setState({ [e.currentTarget.name]: e.currentTarget.value });
+  // };
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.setState({
-      name: '',
-      number: '',
-    });
+    const data = { name, number };
+    onSubmit(data);
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    return (
-      <div className={s.maincontainer}>
-        <form className={s.mainform} onSubmit={this.handleSubmit}>
-          <label htmlFor="inpname" className={s.mainlabel}>
-            Name{' '}
-          </label>
-          <input
-            id="inpname"
-            className={s.mainInput}
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            value={this.state.name}
-            onChange={this.handleInput}
-          />
-          <label htmlFor="inptel" className={s.mainlabel}>
-            Number{' '}
-          </label>
-          <input
-            id="inptel"
-            className={s.mainInput}
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            value={this.state.number}
-            onChange={this.handleInput}
-          />
-          <button type="submit" className={s.submitBtn}>
-            Add contact
-          </button>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div className={s.maincontainer}>
+      <form className={s.mainform} onSubmit={handleSubmit}>
+        <label htmlFor="inpname" className={s.mainlabel}>
+          Name{' '}
+        </label>
+        <input
+          id="inpname"
+          className={s.mainInput}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={name}
+          onChange={e => setName(e.currentTarget.value)}
+        />
+        <label htmlFor="inptel" className={s.mainlabel}>
+          Number{' '}
+        </label>
+        <input
+          id="inptel"
+          className={s.mainInput}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={number}
+          onChange={e => setNumber(e.currentTarget.value)}
+        />
+        <button type="submit" className={s.submitBtn}>
+          Add contact
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default ContactForm;
 
 ContactForm.propTypes = {
-  name: PropTypes.string,
-  number: PropTypes.number,
+  onSubmit: PropTypes.func,
 };
